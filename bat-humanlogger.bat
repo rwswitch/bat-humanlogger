7d800beed7692e5f27992f6f41878fc415ebb007
Robert Lance <rwswitch@users.noreply.github.com>
2014-12-16 16:54:30 -0500

    Cleaned up, added date seperation
    I cleaned up a ton of junk.
    Added separation for entries which then get pushed to a daily master so that I have slightly less chance of being an idiot about it.
    Goal is to have it also accept various fields (output field, working directory, editor, etc.) so that I can port it elsewhere when necessary

--- bat-humanlogger.bat
+++ bat-humanlogger.bat
@@ -1,54 +1,25 @@
-@echo off
-::This is a quick little callnote taking system that was written for the purpose of
-::helping speed up my workflow of repetitive input tasks
-::
-::It pulls today's date and a template file, loads it into a temp file and editor
-::
-::Shortcuts: (Use your editor's find section to edit these)
-::DATETIMEFORMAT - date/time formatting strings
-::
+@ECHO off
+::template file name
+SET templatefile=template.txt
 
-:: template file name, change this to your target template file
+::editor
+SET editor=notepad
 
-set templatefile=template.txt
+::get localtime/date information put into corresponding variables
+FOR /f %%x in ('wmic PATH win32_localtime get /FORMAT:list ^| FINDSTR "="') do SET %%x
 
-::unnecssary indicator of what this does
-set editor=notepad
+::set day and time
+SET today=%year%%month%%day%
+SET currenttime=%hour%%minute%%second%
 
-:: sets up template to be printed into the file I know I could skip this step, but I wanted readable code
+::set current ticket and destination file information
+SET current_ticket_information=%today%-%currenttime%.txt
+SET destination=%today%.txt
+TYPE %templatefile% >> %current_ticket_information%
 
-::todo: add idiot proofing/check for template file
-::if not exist %templatefile%
-::echo on
-::echo template file does not exist...
+::open current ticket for edit
+%editor% %current_ticket_information%
 
+::output to destination
+TYPE %current_ticket_information% >> %destination%
 
-:: sets up date information
-:: example taken from: http://stackoverflow.com/questions/10945572/windows-batch-formatted-date-into-variable
-
-:: Turn this on or copy this for loop to see what the date formatting is like
-@echo off
-for /f %%x in ('wmic path win32_localtime get /format:list ^| findstr "="') do set %%x
-@echo off
-
-:: "DATETIMEFORMAT"
-:: set these to taste
-::
-::
-::
-::
-SET today=%YEAR%%MONTH%%DAY%
-::set currentime=%hour%%minute%
-
-
-:: sets the current logfile to one represented by today
-
-set filename=%today%.txt
-::echo Template:
-::type %templatefile%
-::echo Destination: & echo:%filename%
-type %templatefile% >> %filename%
-
-%editor% %filename%
-
-::PAUSE
