96e5b417606e5408633b307534f728bdb539ce00
Robert Lance <rwswitch@users.noreply.github.com>
2015-04-08 19:32:11 -0400

    changed type to echo to link files
    This is with future plans to make things tie together so I can have a 1 stop edit shop.

--- newticket.cmd
+++ newticket.cmd
@@ -11,5 +11,5 @@ TYPE %templatefile% >> %current_ticket_information%
 %editor% %current_ticket_information%
 
 ::output to destination
-TYPE %current_ticket_information% >> %daylog%
+ECHO %current_ticket_information% >> %daylog%
 
